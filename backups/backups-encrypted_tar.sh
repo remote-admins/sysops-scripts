31db6b6a4b7e80948e671e5b7acc4fd2ef2803e9
remote-admin <58732782+remote-admins@users.noreply.github.com>
2025-03-13 12:39:13 +0200

    Update backups-encrypted_tar.sh

--- backups/backups-encrypted_tar.sh
+++ backups/backups-encrypted_tar.sh
@@ -7,27 +7,27 @@ SALT='your_super-duper-megahard_encryption-string'
 host=$(hostname -s)
 today_folder="$(date +%Y.%m.%d)"
 
-tar=`which tar`
-openssl=`which openssl`
-pigz=`which pigz`
-mkdir=`which mkdir`
+tar=`which tar` || { echo "Failed to find tar"; exit 1; }
+openssl=`which openssl` || { echo "Failed to find openssl"; exit 1; }
+pigz=`which pigz` || { echo "Failed to find pigz"; exit 1; }
+mkdir=`which mkdir` || { echo "Failed to find mkdir"; exit 1; }
 
 # Timestamp for unique tarball and encrypted file names
 TIMESTAMP=$(date +%Y%m%d%H%M%S)
-[ ! -d "$DST_DIR/$today_folder" ] && $mkdir $DST_DIR/$today_folder
+[ ! -d "$DST_DIR/$today_folder" ] && $mkdir $DST_DIR/$today_folder || { echo "Failed to create directory $today_folder"; exit 1; }
 DST_DIR=$DST_DIR/$today_folder
 
-#Create TARBALL
-TARBALL="$DST_DIR/backup_$(basename $SRC_DIR)_$TIMESTAMP.tar.gz"
-$tar --use-compress-program="pigz -p 24 -6" -cf $TARBALL $SRC_DIR
+# Create TARBALL
+TARBALL="backup_$(basename $SRC_DIR)_$TIMESTAMP.tar.gz"
+$tar --use-compress-program="pigz -p 24 -5" -cf $DST_DIR/$TARBALL $SRC_DIR || { echo "Failed to create tarball"; exit 1; }
 
 # Encrypt the tarball using openssl
-ENCRYPTED_TARBALL="$DST_DIR/$TARBALL.tar.gz.enc"
-$openssl enc -aes-256-cbc -salt -in "$TARBALL" -out "$ENCRYPTED_TARBALL" -k "$SALT" -pbkdf2
+ENCRYPTED_TARBALL="$DST_DIR/$TARBALL.enc"
+$openssl enc -aes-256-cbc -salt -in "$DST_DIR/$TARBALL" -out "$ENCRYPTED_TARBALL" -k "$SALT" -pbkdf2 || { echo "Encryption failed"; exit 1; }
 
 # Verify if the encryption was successful then remove the tarball
 if [ $? -eq 0 ]; then
-  rm "$TARBALL"
+  rm "$DST_DIR/$TARBALL" || { echo "Failed to remove tarball"; exit 1; }
   echo "Backup and encryption completed successfully."
 else
   echo "Error during encryption process."
