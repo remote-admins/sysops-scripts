0362b7e6a48c7070af7d97c74b2833abbc7b1fb1
remote-admin <58732782+remote-admins@users.noreply.github.com>
2025-03-13 11:27:02 +0200

    Update backups-encrypted_tar.sh

--- backups/backups-encrypted_tar.sh
+++ backups/backups-encrypted_tar.sh
@@ -4,21 +4,26 @@
 SRC_DIR='{YOUR DIRECTORY WITH FILES FOR BACKUP. USE FULL PATH}'
 DST_DIR='{FULL PATH OF YOUR BACKUPS DIRECTORY, where tarballs backups are located}'
 SALT='your_super-duper-megahard_encryption-string'
+host=$(hostname -s)
+today_folder="$(date +%Y.%m.%d)"
 
 tar=`which tar`
 openssl=`which openssl`
 pigz=`which pigz`
+mkdir=`which mkdir`
 
 # Timestamp for unique tarball and encrypted file names
 TIMESTAMP=$(date +%Y%m%d%H%M%S)
+[ ! -d "$DST_DIR/$today_folder" ] && $mkdir $DST_DIR/$today_folder
+DST_DIR=$DST_DIR/$today_folder
 
-# Create the tarball with pigz compression using 8 parallel processes
-TARBALL="$DST_DIR/backup_$TIMESTAMP.tar.gz"
-$tar -cf - "$SRC_DIR" | $pigz -p 8 > "$TARBALL"
+#Create TARBALL
+TARBALL="$DST_DIR/backup_$(basename $SRC_DIR)_$TIMESTAMP.tar.gz"
+$tar --use-compress-program="pigz -p 24 -6" -cf $TARBALL $SRC_DIR
 
 # Encrypt the tarball using openssl
-ENCRYPTED_TARBALL="$DST_DIR/backup_$TIMESTAMP.tar.gz.enc"
-$openssl enc -aes-256-cbc -salt -in "$TARBALL" -out "$ENCRYPTED_TARBALL" -k "$SALT"
+ENCRYPTED_TARBALL="$DST_DIR/$TARBALL.tar.gz.enc"
+$openssl enc -aes-256-cbc -salt -in "$TARBALL" -out "$ENCRYPTED_TARBALL" -k "$SALT" -pbkdf2
 
 # Verify if the encryption was successful then remove the tarball
 if [ $? -eq 0 ]; then
@@ -28,4 +33,3 @@ else
   echo "Error during encryption process."
   exit 1
 fi
-
